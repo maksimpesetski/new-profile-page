f2d120caacac9e14dd3a176bb654a596bf224da5
Maksim Pesetski <maksim.pesetski@gmail.com>
2019-08-05 22:23:00 -0400

    git commit

--- Builders-TestFirst/03-Array.prototype/07-my-splice/my-splice.js
+++ Builders-TestFirst/03-Array.prototype/07-my-splice/my-splice.js
@@ -1,17 +1,37 @@
-function mySplice (arr, start, deleteCount) {
+// function mySplice (arr, start, deleteCount) {
 
-    let deleted = [];
-    let keep = [];
+//     let deleted = [];
+//     let keep = [];
    
-     if (deleteCount === undefined) deleteCount = arr.length - start;
+//      if (deleteCount === undefined) deleteCount = arr.length - start;
    
-     for (let i = 0; i < start; i++) keep.push(arr.shift());
+//      for (let i = 0; i < start; i++) keep.push(arr.shift());
    
-     for (let x = 0; x < deleteCount; x++) deleted.push(arr.shift());
+//      for (let x = 0; x < deleteCount; x++) deleted.push(arr.shift());
    
-     for (let y = arguments.length - 1; y > 2; y--) arr.unshift(arguments[y]);
+//      for (let y = arguments.length - 1; y > 2; y--) arr.unshift(arguments[y]);
    
-     for (let j = 0; j < keep.length; j++) arr.unshift(keep.pop());
+//      for (let j = 0; j < keep.length; j++) arr.unshift(keep.pop());
    
-    return deleted;
-   }
+//     return deleted;
+//    }
+
+//    const mySlice = (arr, start, end) => {
+//     let copy = [];
+    
+//     if (start < 0) start = arr.length + start;
+//     if (end < 0) end = arr.length + end;
+  
+//       let copy = [];
+  
+//       if (start<0) start = arr.length + start;
+//       if (end<0) end = arr.length + end;
+//     // TODO: check your loop, the browser is "hanging" when testem runs, this indicates there is potentially an infinite loop
+//     // You increment and i, but your evaluated expression "start < end" doesn't use i, this is the root of your problem.
+//     //   for (let i = start; start < end; i++) copy.push(arr[i]);
+  
+//       for (let i=start; i<end; i++) copy.push(arr[i]);
+  
+//       return copy
+//    }
+//     return copy;
